be563ec41825b44513b11f5ada29eb8736f5d8dc
billighost <bb2010ng@gmail.com>
2025-11-01 15:53:45 +0100

    233

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app import create_app, db
 from app.models import User, Project
+import os
+
 
 app = create_app()
 
@@ -7,5 +9,7 @@ app = create_app()
 def make_shell_context():
     return {'db': db, 'User': User, 'Project': Project}
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
